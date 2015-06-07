57bdae2cea5e2a9d7076fdc3bb16dce93e017b5c
Peter Cottle <pcottle@fb.com>
2015-06-07 10:00:08 -0700

    first test

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.registerTask('compliment', 'Stay motivated!', function() {
+    var compliments = grunt.config('compliment.compliments');
+    var index = Math.floor(Math.random() * compliments.length);
+
+    grunt.log.writeln(compliments[index]);
+  });
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compliment: {
+      compliments: [
+        "Wow peter great work!",
+        "Such a professional dev environment",
+        "Can't stop the TRAIN",
+        "git raging"
+      ]
+    },
+    jasmine_node: {
+      projectRoot: './src/js/__tests__/',
+      forceExit: true,
+      verbose: true
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.registerTask('test', ['jasmine_node', 'compliment']);
+};
---
+++ src/js/__tests__/stations.spec.js
@@ -0,0 +1,7 @@
+describe('Stations', function() {
+
+  it('has every station in the order list', function() {
+    expect(1).toBe(1);
+  });
+
+});
