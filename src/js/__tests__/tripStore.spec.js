5f11bc987a6c6e2f0f9b9a1b6f06625672a1013b
Peter Cottle <pcottle@fb.com>
2015-06-30 17:10:07 -0700

    add data for trip store

---
+++ src/js/__tests__/tripStore.spec.js
@@ -0,0 +1,125 @@
+var TripActions = require('../actions/TripActions');
+var TripStore = require('../stores/TripStore');
+
+describe('TripStore', function() {
+
+  it('handles station updates', function() {
+    TripActions.selectDeparture('palo-alto');
+    TripActions.selectArrival('redwood-city');
+
+    expect(TripStore.getDepartureStationID())
+      .toBe('palo-alto');
+
+    expect(TripStore.getArrivalStationID())
+      .toBe('redwood-city');
+
+    // and the favorite trip is set
+    var favoriteTrips = TripStore.getFavoriteTrips();
+    expect(favoriteTrips.length).toBe(1);
+    expect(JSON.stringify(favoriteTrips))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }
+      ]));
+  });
+
+  it('handles multiple fav trips', function() {
+    TripActions.selectDeparture('palo-alto');
+    TripActions.selectArrival('redwood-city');
+
+    expect(JSON.stringify(TripStore.getFavoriteTrips()))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }
+      ]));
+
+    TripActions.selectDeparture('san-francisco');
+    TripActions.selectArrival('palo-alto');
+
+    expect(JSON.stringify(TripStore.getFavoriteTrips()))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'san-francisco',
+          arrivalID: 'palo-alto'
+        }, {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }
+      ]));
+
+    TripActions.selectDeparture('palo-alto');
+    TripActions.selectArrival('redwood-city');
+
+    // simply shuffles
+    expect(JSON.stringify(TripStore.getFavoriteTrips()))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }, {
+          departureID: 'san-francisco',
+          arrivalID: 'palo-alto'
+        }
+      ]));
+
+    TripActions.selectDeparture('palo-alto');
+    TripActions.selectArrival('redwood-city');
+
+    // no change
+    expect(JSON.stringify(TripStore.getFavoriteTrips()))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }, {
+          departureID: 'san-francisco',
+          arrivalID: 'palo-alto'
+        }
+      ]));
+
+    TripActions.selectDeparture('san-francisco');
+    TripActions.selectArrival('california-ave');
+
+    expect(JSON.stringify(TripStore.getFavoriteTrips()))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'san-francisco',
+          arrivalID: 'california-ave'
+        }, {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }, {
+          departureID: 'san-francisco',
+          arrivalID: 'palo-alto'
+        }
+      ]));
+
+    TripActions.selectDeparture('san-francisco');
+    TripActions.selectArrival('22nd-street');
+
+    // only 3
+    expect(JSON.stringify(TripStore.getFavoriteTrips()))
+      .toBe(JSON.stringify([
+        {
+          departureID: 'san-francisco',
+          arrivalID: '22nd-street'
+        }, {
+          departureID: 'san-francisco',
+          arrivalID: 'california-ave'
+        }, {
+          departureID: 'palo-alto',
+          arrivalID: 'redwood-city'
+        }
+      ]));
+  });
+
+  it('prevents bad station IDs', function() {
+    expect(function() {
+      TripActions.selectDeparture('foobarbaz');
+    }).toThrow();
+  });
+});
--- src/js/stores/TripStore.js
+++ src/js/stores/TripStore.js
@@ -6,13 +6,92 @@ var AppConstants = require('../constants/AppConstants');
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
 var Stations = require('../constants/Stations');
+var STATIONS_BY_ID = Stations.__getStationsByID();
 
 var ActionTypes = AppConstants.ActionTypes;
-
-var TRAIN_REQUEST_URI = 'http://caltrain-api.thejsj.com/v1/train';
+var MAX_NUM_FAVORITE_TRIPS = 3;
+var FAVORITE_TRIPS_KEY = 'favorite_trips';
 
 var _departureID = null;
 var _arrivalID = null;
+var _favoriteTrips = [];
+
+// We have this giant hack since this code needs to
+// run in jasmine tests as well
+function _getAsyncStorage() {
+  var React = require('react-native');
+  // cant use ES6 in jasmine
+  return React.AsyncStorage;
+}
+
+function _isSameTrip(tripA, tripB) {
+  return tripA.departureID === tripB.departureID &&
+    tripA.arrivalID === tripB.arrivalID;
+}
+
+function _hasFavoriteTrip(trip) {
+  for (var i = 0; i < _favoriteTrips.length; i++) {
+    if (_isSameTrip(trip, _favoriteTrips[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function _saveTripsToStorage() {
+  var AsyncStorage = _getAsyncStorage();
+  AsyncStorage.setItem(
+    FAVORITE_TRIPS_KEY,
+    JSON.stringify(_favoriteTrips)
+  );
+}
+
+function _tryToSaveFavoriteTripsImpl() {
+  if (_arrivalID === null || _departureID === null) {
+    // Have not selected yet
+    return;
+  }
+
+  var thisTrip = {
+    departureID: _departureID,
+    arrivalID: _arrivalID
+  };
+
+  // Alright both selected -- lets see if we have it...
+  if (_hasFavoriteTrip(thisTrip)) {
+    // Simply move it to the front
+    var newFavoriteTrips = [thisTrip];
+    newFavoriteTrips = newFavoriteTrips.concat(
+      _favoriteTrips.filter(function(trip) {
+        return !_isSameTrip(trip, thisTrip);
+      })
+    );
+    _favoriteTrips = newFavoriteTrips;
+  } else {
+    // add it at the front and slice
+    _favoriteTrips.unshift(thisTrip);
+    _favoriteTrips = _favoriteTrips.slice(0, MAX_NUM_FAVORITE_TRIPS);
+  }
+
+  // Alright now lets try to save
+  _saveTripsToStorage();
+}
+
+function _tryToSaveFavoriteTrips() {
+  try {
+    _tryToSaveFavoriteTripsImpl()
+  } catch (error) {
+    console.warn('Could not save trips');
+    return;
+  }
+}
+
+function _validateStationID(stationID) {
+  if (!STATIONS_BY_ID[stationID]) {
+    throw new Error('bad stationID ', stationID);
+  }
+  return stationID;
+}
 
 var TripStore = assign(
 {},
@@ -28,17 +107,22 @@ AppConstants.StoreSubscribePrototype,
     return _arrivalID;
   },
 
+  getFavoriteTrips: function() {
+    return _favoriteTrips.slice(0);
+  },
+
   dispatchToken: AppDispatcher.register(function(payload) {
     var action = payload.action;
     var shouldInform = false;
 
     switch (action.type) {
       case ActionTypes.SELECT_DEPARTURE:
-        _departureID = action.stationID;
+        _departureID = _validateStationID(action.stationID);
         shouldInform = true;
         break;
       case ActionTypes.SELECT_ARRIVAL:
-        _arrivalID = action.stationID;
+        _arrivalID = _validateStationID(action.stationID);
+        _tryToSaveFavoriteTrips();
         shouldInform = true;
         break;
     }
